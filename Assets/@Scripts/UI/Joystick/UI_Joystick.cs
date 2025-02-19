3ce3334dc4f64b0a97561c179ad6a84b3d15033e
carrot4343 <82187527+carrot4343@users.noreply.github.com>
2025-02-19 21:04:02 +0900

    fixing

--- Assets/@Scripts/UI/Joystick/UI_Joystick.cs
+++ Assets/@Scripts/UI/Joystick/UI_Joystick.cs
@@ -10,38 +10,20 @@ public class UI_Joystick : MonoBehaviour, IPointerClickHandler, IPointerDownHand
     [SerializeField] Image m_handler;
     [SerializeField] GameObject m_gameScene;
 
-    RectTransform m_backgroundScreen;
+    GameObject m_backgroundScreen;
     Vector2 m_touchPosition;
     Vector2 m_moveDir;
     float m_joystickRadius;
 
     void Start()
     {
-        m_backgroundScreen = Utils.FindChild(m_gameScene, "Background").GetComponent<RectTransform>();
+        m_backgroundScreen = Utils.FindChild(m_gameScene, "Background");
         m_joystickRadius = m_backGround.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
-
-        m_backgroundScreen.sizeDelta = new Vector2(0, 150); // 가로 0, 세로 150
-        m_backgroundScreen.anchoredPosition = new Vector2(0, -75); // 위치 조정
     }
 
     void Update()
     {
-        
-    }
-    void OnDrawGizmos()
-    {
-        if (m_backgroundScreen == null)
-            return;
 
-        // RectTransform의 네 모서리 좌표 계산
-        Vector3[] corners = new Vector3[4];
-        m_backgroundScreen.GetWorldCorners(corners);
-
-        // 네 모서리를 선으로 그리기
-        Debug.DrawLine(corners[0], corners[1], Color.red); // 왼쪽 아래 -> 오른쪽 아래
-        Debug.DrawLine(corners[1], corners[2], Color.red); // 오른쪽 아래 -> 오른쪽 위
-        Debug.DrawLine(corners[2], corners[3], Color.red); // 오른쪽 위 -> 왼쪽 위
-        Debug.DrawLine(corners[3], corners[0], Color.red); // 왼쪽 위 -> 왼쪽 아래
     }
 
     public void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
@@ -50,10 +32,7 @@ public class UI_Joystick : MonoBehaviour, IPointerClickHandler, IPointerDownHand
     }
     public void OnPointerDown(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        Debug.Log("Size: " + m_backgroundScreen.sizeDelta);
-        Debug.Log("Position: " + m_backgroundScreen.anchoredPosition);
-
-        if (RectTransformUtility.RectangleContainsScreenPoint(m_backgroundScreen, eventData.position, null) == true)
+        if(eventData.hovered.Contains(m_backgroundScreen))
         {
             Debug.Log(eventData.position);
         }
